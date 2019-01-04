67639af192bd04e26fcb70b178d0e92213afddd2
Hernán Cote <hernan.cote@outlook.com>
2019-01-04 09:20:18 -0500

    Eliminated raven dependencies

--- src/services/logServices.js
+++ src/services/logServices.js
@@ -1,13 +1,9 @@
-import Raven from "raven-js";
-import { ravenUrl } from "../config.json";
-
 function init() {
-  const options = { release: "1.0.0", environment: "development-test" };
-  Raven.config(ravenUrl, options).install();
+  console.log('Initialized logs');
 }
 
 function log(error) {
-  Raven.captureException(error);
+  console.log(error);
 }
 
 export default {
